03d64f2cb365b8796f9c4d3b5e5404454018bbe2
tjmashu <1078855084@qq.com>
2019-02-23 14:50:14 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@
     // 页面顶部滚进去的距离
     var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
 
-    if (scrollTop > 200) {
+    if (scrollTop > 80) {
       backToTop.classList.add('back-to-top-show')
     } else {
       backToTop.classList.remove('back-to-top-show')
